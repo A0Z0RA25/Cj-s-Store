39eb0e5e15cdcc0493b6a8af1e37cc39c8e066f7
A0Z0RA25 <98691018+A0Z0RA25@users.noreply.github.com>
2024-04-06 10:58:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ const about = document.querySelector("#about");
 const container1 = document.querySelector(".home-page");
 const container2 = document.querySelector(".about-page");
 
+
 home.addEventListener('click', showHome);
 function showHome(){
+
+    var element = document.querySelector("li");
+    element.classList.add("animate__hinge");
+
     if(container1.style.display = "none"){
         container1.style.display = "block";
-        container2.style.display = "none"
+        container2.style.display = "none";
     } else {
-        conatiner.style.display = "block";
+        container1.style.display = "block";
     }
 }
 
 about.addEventListener('click', showAbout);
 function showAbout(){
+
+
+    var element = document.querySelector("li");
+    element.classList.add("animate__hinge");
+
     if(container2.style.display = "none"){
         container2.style.display = "block";
         container1.style.display = "none"
@@ -22,3 +32,27 @@ function showAbout(){
         container2.style.display = "block";
     }
 }
+//
+let person = prompt("Enter your name");
+
+if (person === "CJ"){
+    const para = document.createElement("p");
+    const node = document.createTextNode(`jhjhkHdbbuibvu si ${person}`);
+    para.appendChild(node);
+    const strat = document.querySelector(".strat");
+    strat.appendChild(para);
+} else {
+    const para = document.createElement("p");
+    const node = document.createTextNode(`Naapaka mabit na bata nan si ${person}`);
+    para.appendChild(node);
+    const strat = document.querySelector(".strat");
+    strat.appendChild(para);
+}
+
+const closeBtn = document.querySelector(".close");
+
+closeBtn.addEventListener('click', () =>{
+    document.querySelector(".strat").style.display = "none";
+});
+
+//
